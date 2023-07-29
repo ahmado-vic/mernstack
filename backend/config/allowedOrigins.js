a67b9abc3e18e693c89569670d371408731e2e99
Ahmad Hussein <ahussin8@gmail.com>
2023-07-29 14:21:59 +0300

    Update allowedOrigins.js
    modify allowed origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,5 @@
 const allowedOrigins = [
-  'https://technotes-api.onrender.com',
-  'http://localhost:3500',
-  'http://127.0.0.1:3500',
-  'https://www.yourwebsite.com',
-  'https://www.google.com',
+  'https://technotes-api-nmfi.onrender.com',
 ];
 
 module.exports = allowedOrigins;
